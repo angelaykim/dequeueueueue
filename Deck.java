05a301a78af440898d59ddaf58995ece696c879d
Eric Chen <echen8@stuy.edu>
2017-04-03 10:57:11 -0400

    finished Deck.java

--- Deck.java
+++ Deck.java
@@ -1,10 +1,30 @@
 public class Deck<T> implements Deque {
+
     private DLLNode<T> _head, _tail;
     private int _size;
+
     public Deck() {
 	_size = 0;
 	_head = _tail = null;
     }
+
+    public boolean add(T t) {
+	addLast(t);
+	return true;
+    }
+
+    public void addFirst( T t ) {
+	if ( isEmpty() ) {
+	    _head = _tail = new DLLNode<T>( t, null, null );
+	}
+	else {
+	    DLLNode<T> tmp = new DLLNode<T>( t, _head, null );
+	    _head.setPrev( tmp );
+	    _head = tmp;
+	}
+	_size++;
+    }
+
     public void addLast(T t) {
 	if(isEmpty())
 	    _head = _tail = new DLLNode<T>(t, null, null);
@@ -14,16 +34,39 @@ public class Deck<T> implements Deque {
 	}
 	_size++;
     }
-    public boolean add(T t) {
-	addLast(t);
-	return true;
+
+    public T peekFirst() {
+	return _head.getCargo();
     }
+
+    public T peekLast() {
+	return _tail.getCargo();
+    }
+
+    public T pollFirst() {
+	T tmp = _head.getCargo();
+	_head = _head.getNext();
+	_head.setPrev( null );
+	_size--;
+	return tmp;
+    }
+
+    public T pollLast() {
+	T tmp = _tail.getCargo();
+	_tail = _tail.getPrev();
+	_tail.setNext( null );
+	_size--;
+	return tmp;
+    }
+	
     public boolean isEmpty() {
 	return _size==0;
     }
+
     public int size() {
 	return _size;
     }
+
     public String toString() {
 	String retStr = "";
 	DLLNode<T> temp = _head;
@@ -33,6 +76,7 @@ public class Deck<T> implements Deque {
 	}
 	return retStr;
     }
+
     public static void main(String args[]) {
 	Deck<String> dock = new Deck<String>();
 	dock.add("hello");
