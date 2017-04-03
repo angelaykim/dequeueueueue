30054fcd0aa6373a7f9093c2945ce31042a1d0f4
anotherLostKitten <tpeters@stuy.edu>
2017-04-03 10:47:22 -0400

    added toString, add, addFirst, size, and isEmpty

---
+++ Deck.java
@@ -0,0 +1,44 @@
+public class Deck<T> implements Deque {
+    private DLLNode<T> _head, _tail;
+    private int _size;
+    public Deck() {
+	_size = 0;
+	_head = _tail = null;
+    }
+    public void addLast(T t) {
+	if(isEmpty())
+	    _head = _tail = new DLLNode<T>(t, null, null);
+	else {
+	    _tail.setNext(new DLLNode<T>(t, null, _tail));
+	    _tail = _tail.getNext();
+	}
+	_size++;
+    }
+    public boolean add(T t) {
+	addLast(t);
+	return true;
+    }
+    public boolean isEmpty() {
+	return _size==0;
+    }
+    public int size() {
+	return _size;
+    }
+    public String toString() {
+	String retStr = "";
+	DLLNode<T> temp = _head;
+	while (temp!=null) {
+	    retStr+=temp.toString() + " ";
+	    temp = temp.getNext();
+	}
+	return retStr;
+    }
+    public static void main(String args[]) {
+	Deck<String> dock = new Deck<String>();
+	dock.add("hello");
+	dock.add("friends");
+	dock.add("and");
+	dock.add("compatriots");
+	System.out.println(dock);
+    }
+}
